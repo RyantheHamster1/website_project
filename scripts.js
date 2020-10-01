d19d9c68df92d37e197d5ab6ef763886d959bb72
RyantheHamster1 <70215578+RyantheHamster1@users.noreply.github.com>
2020-09-30 23:10:18 -0400

    Made the button look a bit nicer

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
             $("img").animate({left: '1700px'});
             starting = false
         } else {
-            $("img").animate({left: '458px'});
+            $("img").animate({left: '469px'});
             starting = true
         }
         
